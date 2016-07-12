e7329876b24fd48460be22db8a12f37cd298eca0
rkulak <r.kulak@smartweb.com.ua>
2016-07-12 13:49:01 +0300

    template and file upload

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,10 @@
 import flask
+from flask_bootstrap import Bootstrap
 from .views import setup_routes
 
 
 def main():
     app = flask.Flask(__name__)
+    bootstrap = Bootstrap(app)
     setup_routes(app)
     app.run()
