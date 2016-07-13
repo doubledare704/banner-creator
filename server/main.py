1d19e2ac0579c19b1e5e9bf05039b188ecf06ccf
Yurii Karabadzhak <y.karabadzhan@smartweb.com.ua>
2016-07-13 17:37:36 +0300

    Add DB migration tool

--- server/main.py
+++ server/main.py
@@ -1,5 +1,9 @@
 import flask
+
 from flask_bootstrap import Bootstrap
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
 from server.views import setup_routes
 from server.db import db
 
@@ -10,11 +14,19 @@ def main():
     app = flask.Flask(__name__, instance_relative_config=True)
     setup_routes(app)
 
-    # Apply default config and dev config from instance/config.cfg if exists
+    # Apply default config and dev config from instance/config.py if exists
     app.config.from_object('server.config')
-    app.config.from_pyfile('config.cfg', silent=True)
+    app.config.from_pyfile('config.py', silent=True)
 
     bootstrap.init_app(app)
     db.init_app(app)
 
-    app.run()
+    # Load all models to be available for db migration tool
+    from server import models
+
+
+    migrate = Migrate(app, db, directory='server/migrations')
+    manager = Manager(app)
+    manager.add_command('db', MigrateCommand)
+
+    manager.run()
