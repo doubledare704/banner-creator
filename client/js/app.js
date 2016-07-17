07c0d53faacae5eae0049ae254da5a63bdccb0f0
Roman Kulak <rome1407@gmail.com>
2016-07-17 21:36:36 +0300

    react.js added

--- client/js/app.js
+++ client/js/app.js
@@ -1,2 +1,18 @@
 var fabric = require('./fabmain.js');
 var styluses = require('./../css/main.styl');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ var Header = React.createClass({
+  render: function() {
+    return (
+      <h1>Upload new File</h1>
+    );
+  }
+});
+ReactDOM.render(
+  <Header />,
+  document.getElementById('content')
+);
+
+
