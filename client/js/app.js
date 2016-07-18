c2e60c4d203fa64e4f8b815c2295ea9675b7fc85
oleksiy <doubledare704@gmail.com>
2016-07-18 15:23:43 +0300

    editor adding

--- client/js/app.js
+++ client/js/app.js
@@ -1,18 +1,3 @@
+var editor = require('./editor.js');
 var fabric = require('./fabmain.js');
 var styluses = require('./../css/main.styl');
-var React = require('react');
-var ReactDOM = require('react-dom');
-
- var Header = React.createClass({
-  render: function() {
-    return (
-      <h1>Upload new File</h1>
-    );
-  }
-});
-ReactDOM.render(
-  <Header />,
-  document.getElementById('content')
-);
-
-
