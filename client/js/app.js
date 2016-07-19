8a8c38c462fe2a976844013fdf09c1ae6c5a517b
oleksiy <doubledare704@gmail.com>
2016-07-19 15:44:50 +0300

    delete unused js
    clear up fabmain
    add new funcs into editor

--- client/js/app.js
+++ client/js/app.js
@@ -1,8 +1,8 @@
 import inactiveImg from './admin/inactiveImg.js'
 import deleteImg from './admin/deleteFromDB.js'
+import fabric from './fabmain'
 
-var editor = require('./editor.js');
-var fabric = require('./fabmain.js');
+// var editor = require('./editor.js');
 var styluses = require('./../css/main.styl');
 var Baz = require('bazooka');
 
@@ -10,6 +10,7 @@ var Baz = require('bazooka');
 Baz.register({
     'inactiveImg': inactiveImg,
     'deleteImg': deleteImg,
+    'fabric': fabric
 });
 
 
