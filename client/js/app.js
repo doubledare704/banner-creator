67b0e7db75f55db3eee78f4b9d1cd35fba702a9b
Yurii Karabadzhak <y.karabadzhan@smartweb.com.ua>
2016-07-20 16:56:06 +0300

    Refactor code for background images' loading

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,7 @@
 import inactiveImg from './admin/inactiveImg.js'
 import deleteImg from './admin/deleteFromDB.js'
 import fabric from './fabmain'
+import {openBackgroundsList, setBackground, loadBackgroundImages} from './background'
 
 // var editor = require('./editor.js');
 var styluses = require('./../css/main.styl');
@@ -10,7 +11,10 @@ var Baz = require('bazooka');
 Baz.register({
     'inactiveImg': inactiveImg,
     'deleteImg': deleteImg,
-    'fabric': fabric
+    'fabric': fabric,
+    'openBackgroundsList': openBackgroundsList,
+    'setBackground': setBackground,
+    'loadBackgroundImages': loadBackgroundImages
 });
 
 var unwatch = Baz.watch();
