0ebf574f3796cfc3df77674b0c48f15a8465f777
Николай Коропатник <koropatnik@yandex.ru>
2016-07-27 18:13:52 +0300

    auth work in popup window, logout dynamic

--- client/js/app.js
+++ client/js/app.js
@@ -1,12 +1,14 @@
 import renderImages from './images/renderImages.jsx';
 import backgroundsAdmin from './admin/backgroundsAdmin.jsx';
+import loginClick from './auth_helper'
 
 const styluses = require('./../css/main.styl');
 const Baz = require('bazooka');
 
 Baz.register({
     'backgroundsAdmin': backgroundsAdmin,
-    'renderImages': renderImages
+    'renderImages': renderImages,
+    'loginClick': loginClick
 });
 
 
