d093dde65897b9727b56a8209b702296403a51a7
Yurii Karabadzhak <y.karabadzhan@smartweb.com.ua>
2016-07-29 18:00:31 +0300

    Tweak show/hide banner functionality on dashboard

--- client/js/app.js
+++ client/js/app.js
@@ -1,12 +1,14 @@
 import renderImages from './images/renderImages.jsx';
 import backgroundsAdmin from './admin/backgroundsAdmin.jsx';
+import showBanner from './dashboard/dashboard';
 
 const styluses = require('./../css/main.styl');
 const Baz = require('bazooka');
 
 Baz.register({
     'backgroundsAdmin': backgroundsAdmin,
-    'renderImages': renderImages
+    'renderImages': renderImages,
+    'showBanner': showBanner
 });
 
 
