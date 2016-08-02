1a741030d067ed7da512dc545813a1ee2f7f6a8d
oleksiy <doubledare704@gmail.com>
2016-08-02 15:32:45 +0300

    change controls color to black
    back in black

--- client/js/editor/editor.js
+++ client/js/editor/editor.js
@@ -117,6 +117,12 @@ function createLine(points) {
 }
 // end
 
+fabric.Object.prototype.set({
+    transparentCorners: false,
+    borderColor: '#000',
+    cornerColor: '#000'
+});
+
 // make editor
 export default class Editor {
     constructor(canvas, width, height) {
@@ -193,7 +199,7 @@ export default class Editor {
         });
     }
 
-    addButton(w=220, h=80, fontFamily='Roboto', fontSize=20, fontText='Сюда пиши') {
+    addButton(w = 220, h = 80, fontFamily = 'Roboto', fontSize = 20, fontText = 'Сюда пиши') {
         let border = new fabric.Rect({
             width: w,
             height: h,
@@ -203,17 +209,17 @@ export default class Editor {
             rx: 5,
             ry: 5
         });
-        let texting = new fabric.IText(fontText,{
+        let texting = new fabric.IText(fontText, {
             fontFamily: fontFamily,
             fontSize: fontSize,
-            top:h/4,
-            left:w/4.4
+            top: h / 4,
+            left: w / 4.4
         });
-        texting.setTop(h/2 - texting.getHeight()/2);
-        texting.setLeft(w/2 - texting.getWidth()/2);
-        let group = new fabric.Group([border, texting],{
-            left:200,
-            top:100
+        texting.setTop(h / 2 - texting.getHeight() / 2);
+        texting.setLeft(w / 2 - texting.getWidth() / 2);
+        let group = new fabric.Group([border, texting], {
+            left: 200,
+            top: 100
         });
         this.canv.add(group);
     }
@@ -272,12 +278,12 @@ export default class Editor {
         }))
     }
 
-    setTextInItext(texter){
+    setTextInItext(texter) {
         let act = this.canv.getActiveObject();
-        if (act){
+        if (act) {
             let objs = act.getObjects();
-            for (let i =0; i<objs.length; i++){
-                if(objs[i].text){
+            for (let i = 0; i < objs.length; i++) {
+                if (objs[i].text) {
                     objs[i].setText(texter);
                 }
             }
@@ -467,6 +473,7 @@ export default class Editor {
         }
     }
 }
+
 
 export function disableControls(obj, groups) {
     if (obj) {
