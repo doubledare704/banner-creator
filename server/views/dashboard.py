69c912338d06b3df636a18e0d79f43e2fb55f1e3
Yurii Karabadzhak <y.karabadzhan@smartweb.com.ua>
2016-08-02 15:42:52 +0300

    Add pagination for review. Add jinja2 macro and styles for pagination component

--- server/views/dashboard.py
+++ server/views/dashboard.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 
 from flask_login import current_user, login_required
 
@@ -7,14 +7,17 @@ from server.models import User, BannerReview, Banner
 
 @login_required
 def dashboard():
+    page = int(request.args.get('page', 1))  # get page number from url query string
     if current_user.role == User.UserRole.user:
-        reviews = BannerReview.query.filter_by(user_id=current_user.id).order_by(BannerReview.created_at.desc())
+        reviews = BannerReview.query.filter_by(user_id=current_user.id).order_by(BannerReview.created_at.desc()
+                                                                                 ).paginate(page=page, per_page=10)
         return render_template('user/user_dashboard.html', reviews=reviews)
     elif current_user.role == User.UserRole.designer:
         reviews = BannerReview.query.filter_by(designer_id=current_user.id).order_by(BannerReview.created_at.desc())
         return render_template('user/designer_dashboard.html', reviews=reviews)
 
 
+@login_required
 def user_banners():
     banners = Banner.query.filter_by(user=current_user)
     return render_template('user/user_banners.html', banners=banners)
